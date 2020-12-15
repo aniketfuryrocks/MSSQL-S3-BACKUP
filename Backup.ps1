6e3a519180aa7a9baa9461e454857903c09414da
aniketfuryrocks <prajapati.ani306@gmail.com>
2020-12-15 22:18:35 +0530

    error

--- Backup.ps1
+++ Backup.ps1
@@ -1,4 +1,16 @@
 $ErrorActionPreference = "Stop"
+function exitOnError {
+    param (
+        [string[]] $Params
+    )
+    
+    if ($LastExitCode -ne 0){
+        foreach ($Param in $Params){
+            Write-Host $Param -ForegroundColor red
+        }
+        exit 1
+    }
+}
 
 if (!$args[0]) {
     Write-Host "Please specify Backup Path" -ForegroundColor red
@@ -30,17 +42,20 @@ New-Item -ItemType directory -Path $folderPath
 $folderPath = $folderPath | Resolve-Path
 
 # Backup
-Write-Host "Backing up to Dir :" $folderPath -ForegroundColor green
+Write-Host "Backing up to $folderPath" -ForegroundColor green
 sqlcmd -Q "DECLARE @name VARCHAR(50) DECLARE @path VARCHAR(256) DECLARE @fileName VARCHAR(256) SET @path = '$($folderPath)\' DECLARE db_cursor CURSOR READ_ONLY FOR SELECT name FROM master.sys.databases WHERE name NOT IN ('master','model','msdb','tempdb') AND state = 0 AND is_in_standby = 0 OPEN db_cursor FETCH NEXT FROM db_cursor INTO @name WHILE @@FETCH_STATUS = 0 BEGIN SET @fileName = @path + @name + '.BAK' BACKUP DATABASE @name TO DISK = @fileName; FETCH NEXT FROM db_cursor INTO @name END CLOSE db_cursor DEALLOCATE db_cursor"
+exitOnError -Params "Error Backing up to $folderPath" 
 
 # Compress
 $zipPath = -join($folderPath,".7z")
 Write-Host "Compressing Backup to file" $zipPath -ForegroundColor green
 7z a $($zipPath) "$folderPath\*"
+exitOnError -Params "Error Compressing Backup to file $zipPath"
 
 # Copy To AWS
-Write-Host "Pushing zip to s3" -ForegroundColor green
-aws s3 cp $zipPath s3://$args[1]
+Write-Host "Pushing $zipPath to s3" -ForegroundColor green
+aws s3 cp $zipPath "s3://$($args[1])"
+exitOnError -Params "Error pushing $zipPath to s3"
 
 # Removing Folder
 Write-Host "Removing folder" -ForegroundColor green
